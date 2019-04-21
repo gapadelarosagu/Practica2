4f8c2e6a780f2e0d10172b15e25c7101f7cfddd1
GABRIELA DE LA ROSA <gapadelarosagu@ittepic.edu.mx>
2019-04-20 23:44:42 -0600

    Segundo

--- app/src/main/java/mx/edu/ittepic/tpdm_u3_practica2_delarosaguerrero/MainActivity.java
+++ app/src/main/java/mx/edu/ittepic/tpdm_u3_practica2_delarosaguerrero/MainActivity.java
@@ -1,7 +1,9 @@
 package mx.edu.ittepic.tpdm_u3_practica2_delarosaguerrero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
 
         escul = findViewById(R.id.button);
         pintu = findViewById(R.id.button2);
+        pintu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(MainActivity.this,Main4Activity.class);
+                startActivity(i);
+            }
+        });
+        escul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(MainActivity.this,Main5Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
